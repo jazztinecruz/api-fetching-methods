6ed53e4fa94838128a75a12312c1fbb9075c4941
jazztinecruz <jzztn.crz@gmail.com>
2022-07-12 20:13:17 +0800

    Add navbar and logo link

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link"
+import NavLinks from "../section/navlinks"
+import NavLink from "../section/navlinks/navlink"
+
 const NavBar = () => {
   return (
-    <nav>
-      <h1 className='text-lg font-black tracking-wider'>Archives</h1>
+    <nav className='flex items-center gap-10'>
+      <Link href='/'><h1 className='text-lg font-black tracking-wider cursor-pointer'>Archives</h1></Link>
+
+      {/* navigation links */}
+      <NavLinks>
+        <NavLink name="CSR" url="/client-side-rendering"/>
+        <NavLink name="SSR" url="/server-side-rendering"/>
+        <NavLink name="SSG" url="/static-site-generation"/>
+      </NavLinks>
     </nav>
   )
 }

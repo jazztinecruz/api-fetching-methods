176c0cbee84986de9db507dfaf7f48f736f5939e
jazztinecruz <jzztn.crz@gmail.com>
2022-07-12 22:28:46 +0800

    Add second layer

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -7,10 +7,14 @@ interface Props {
 const Layout = ({ children }: Props) => {
   return (
     <div className="h-screen grid grid-rows-[auto,1fr] gap-10">
-      <header className='sticky top-0 z-50 py-4 px-6 bg-white shadow-md shadow-gray-200'>
-        <NavBar />
+      <header className="sticky top-0 z-50 py-4 px-6 bg-white shadow-md shadow-gray-200">
+        <div>
+          <NavBar />
+        </div>
       </header>
-      <main className="py-3 px-2 mx-auto max-w-3xl">{children}</main>
+      <main>
+        <div className="h-full py-3 px-2 max-w-3xl mx-auto grid gap-8">{children}</div>
+      </main>
     </div>
   )
 }
